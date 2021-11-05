be9674aa14b33126fc86b23a48ce4442589aa24a
Stefyfresh <90330171+StefyfreshSchool@users.noreply.github.com>
2021-11-05 14:27:00 -0400

    original code for people who want the buttons

---
+++ programs/scripts/apClassroom/modifyAssignmentCarouselOriginal.js
@@ -0,0 +1,23 @@
+// This version does not remove the buttons.
+console.log("[AP Classroom Assignments Modify (V2.0)]: Page loaded and script loaded, waiting for element.");
+let check = true;
+modifyCarousel = setInterval(()=>{
+    // Assignment carousel element is the first element with class "carousel-parent-container".
+    if (document.getElementsByClassName("carousel-parent-container")[0] != undefined){
+        if (check == true){
+            // If we are checking and the element exists, enable the scroll bar and stop checking.
+            document.getElementsByClassName("carousel-parent-container")[0].style.overflowX = "scroll";
+            console.log("[AP Classroom Assignments Modify (V2.0)]: Carousel found! Adding scroll bar.");
+            
+            check = false;
+        }
+    } else {
+        // Only log to the console the first time the element disappears.
+        if (check == false){
+            console.log("[AP Classroom Assignments Modify (V2.0)]: Carousel disappeared! Waiting for element.");
+        }
+      
+        // If the element does not exist we should check for it.
+        check = true;
+    }
+}, 500);
