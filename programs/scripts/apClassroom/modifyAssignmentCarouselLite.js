ea110adc11a58d958d22ec8997fc37bfe19dd08f
Stefyfresh <90330171+StefyfreshSchool@users.noreply.github.com>
2021-11-05 14:53:35 -0400

    add lite console message

--- programs/scripts/apClassroom/modifyAssignmentCarouselLite.js
+++ programs/scripts/apClassroom/modifyAssignmentCarouselLite.js
@@ -1,11 +1,11 @@
 // This version refreshes 4 times less frequently, meaning it should be better on slower connections/computers. 
 // It is also minified, so check the original for better spacing and comments.
-console.log("[AP Classroom Assignments Modify (V2.1)]: Page loaded and script loaded, waiting for element.");
+console.log("[AP Classroom Assignments Modify (V2.1 LITE)]: Page loaded and script loaded, waiting for element.");
 let check = true;
 modifyCarousel = setInterval(()=>{
     if (document.getElementsByClassName("carousel-parent-container")[0] != undefined){
         if (check == true){
-            console.log("[AP Classroom Assignments Modify (V2.1)]: Carousel found! Adding scroll bar and removing buttons.");
+            console.log("[AP Classroom Assignments Modify (V2.1 LITE)]: Carousel found! Adding scroll bar and removing buttons.");
             document.getElementsByClassName("carousel-parent-container")[0].style.overflowX = "scroll";
             document.getElementsByClassName("slider-decorator-0")[0].outerHTML = "";
             document.getElementsByClassName("slider-decorator-1")[0].outerHTML = "";
@@ -13,7 +13,7 @@ modifyCarousel = setInterval(()=>{
             check = false;
         }
     } else {
-        if (check == false) console.log("[AP Classroom Assignments Modify (V2.1)]: Carousel disappeared! Waiting for element.");
+        if (check == false) console.log("[AP Classroom Assignments Modify (V2.1 LITE)]: Carousel disappeared! Waiting for element.");
         check = true;
     }
 }, 1000);
